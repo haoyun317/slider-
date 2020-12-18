27abc7f0195123fa479390dd95f777507e9f72c6
HaoYun <73008649+haoyun317@users.noreply.github.com>
2020-12-18 11:19:01 +0800

    Update ViewController.swift

--- slider練習物體靠近/ViewController.swift
+++ slider練習物體靠近/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var number: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func slider(_ sender: UISlider) {
+        var leftImageViewOriginX = leftImageView.frame.origin.x
+        var rightImageViewOriginX = rightImageView.frame.origin.x
+        leftImageView.transform = CGAffineTransform(translationX: CGFloat(85 * sender.value), y: 0)
+        rightImageView.transform = CGAffineTransform(translationX: CGFloat(-85 * sender.value), y: 0)
+        rightImageView.alpha = 0
+        rightImageView.alpha = CGFloat(sender.value / sender.maximumValue)
+        let distance = Int(sender.value.rounded())
+        number.text = "小新與" + distance.description + "隻小白的距離"
+    }
+    
 }
 
